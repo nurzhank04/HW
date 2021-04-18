3aed2e0d4018e0cd4acc71065d5a510fc7229db3
nurzhank04 <200103482@stu.sdu.edu.kz>
2021-04-18 22:13:04 +0500

    Initial commit

---
+++ src/HW21_6.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.*;
+
+public class HW21_6 {
+    public static void main(String[] args) throws Exception {
+
+            }
+        }
+
