e870eb01b32a020d48ccac203905da28226be90c
nurzhank04 <200103482@stu.sdu.edu.kz>
2021-04-18 22:31:55 +0500

    init

--- src/HW21_6.java
+++ src/HW21_6.java
@@ -3,8 +3,12 @@ import java.io.FileReader;
 import java.util.*;
 
 public class HW21_6 {
-    public static void main(String[] args) throws Exception {
-
+    public static void main(String[] args){
+                 Scanner sc = new Scanner(System.in);
+                 int n = sc.nextInt();
+                 for(int i = 0;i < n; i++){
+                     System.out.println(i + " ");
+                 }
             }
         }
 
